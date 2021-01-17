4df5da8b080ac8d7dd8bd537daea30f35ae08c57
Marcelo <marcelo99henrique@gmail.com>
2021-01-17 19:49:56 -0300

    End of Section 8

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -1,5 +1,7 @@
 using API.Data;
+using API.Helpers;
 using API.Interfaces;
+using AutoMapper;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -12,6 +14,8 @@ namespace Extensions
     public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config)
     {
       services.AddScoped<ITokenService, TokenService>();
+      services.AddScoped<IUserRepository, UserRepository>();
+      services.AddAutoMapper(typeof(AutoMapperProfiles).Assembly);
       services.AddDbContext<DataContext>(options =>
       {
         options.UseSqlServer(config.GetConnectionString("DefaultConnection"));
---
+++ API/Helpers/AutoMapperProfiles.cs
@@ -0,0 +1,33 @@
+using System.Xml.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Linq;
+using API.DTOs;
+using API.Entities;
+using AutoMapper;
+using API.Extensions;
+
+namespace API.Helpers
+{
+  public class AutoMapperProfiles : Profile
+  {
+    public AutoMapperProfiles()
+    {
+      CreateMap<AppUser, MemberDto>()
+      .ForMember(
+        dest => dest.PhotoUrl,
+        opt => opt.MapFrom
+        (
+          src => src.Photos.FirstOrDefault(x => x.IsMain).Url)
+        )
+      .ForMember(
+        dest => dest.Age,
+        opt => opt.MapFrom
+        (
+          src => src.DateofBirth.CalculateAge()
+        )
+      );
+      CreateMap<Photo, PhotoDto>();
+
+    }
+  }
+}
