d558e95cf8a5bba596841dc60f9f41ff0e3aad50
Marcelo <marcelo99henrique@gmail.com>
2021-01-26 21:46:14 -0300

    End of Section 9

--- API/client/src/app/_modules/shared.module.ts
+++ API/client/src/app/_modules/shared.module.ts
@@ -2,7 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ToastrModule, ToastrService } from 'ngx-toastr';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
-
+import { TabsModule } from 'ngx-bootstrap/tabs';
+import { NgxGalleryModule } from '@kolkov/ngx-gallery';
 
 
 @NgModule({
@@ -11,12 +12,16 @@ import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
     CommonModule,
     BsDropdownModule.forRoot(),
     ToastrModule.forRoot({
-      positionClass:'toast-bottom-right'
-    })
+      positionClass: 'toast-bottom-right'
+    }),
+    TabsModule.forRoot(),
+    NgxGalleryModule
   ],
-  exports:[
+  exports: [
     BsDropdownModule,
-    ToastrModule
+    ToastrModule,
+    TabsModule,
+    NgxGalleryModule
   ]
 })
 export class SharedModule { }
